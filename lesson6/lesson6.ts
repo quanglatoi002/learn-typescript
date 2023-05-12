8f28d4a822bdae9926498365b1417495ba7ca282
quang <quanglatoi002@gmail.com>
2023-05-12 12:57:41 +0700

    lesson6(Array)

---
+++ lesson6/lesson6.ts
@@ -0,0 +1,14 @@
+export {};
+//Nó cũng có 2 kiểu dữ liệu
+//t1 kiểu nguyên thủy (primitive data-type)
+let count: number;
+count = 1;
+
+let count1: number = 2;
+console.log(count1);
+
+//t2 kiểu dữ liệu tham chiếu - References data types:
+let arrName: string[] = ["a", "b", "c", "d"];
+arrName.push("24");
+
+console.log(arrName);
